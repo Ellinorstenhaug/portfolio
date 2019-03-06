9d7c2e92e90d1adb4775b82a162b57a5bf07fdd1
Ellinor <ellinor.stenhaug@gmail.com>
2019-03-06 21:08:07 +0100

    merge

--- src/components/comp/childcomponents/introText.vue
+++ src/components/comp/childcomponents/introText.vue
@@ -2,6 +2,9 @@
     <section class="mb-5">
 
         <v-layout column align-center justify-center>
+
+
+            
             <div class="display-1 text-dark-bg white--text font-weight-medium text-xs-center">Lorem ipsum dolor, sit
                 amet consectetur adipisicing elit. </div> <br>
             <div class="display-1 text-dark-bg white--text font-weight-black mb-3 text-xs-center"> Radipiscing elit, sed
